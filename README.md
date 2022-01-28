55874ba648760b685a99d5e8b18cc1a7c06172f7
Alexandre <alexandre@clusterlab.com.br>
2022-01-28 16:54:42 -0300

    added some useful links

--- README.md
+++ README.md
@@ -9,3 +9,9 @@
 * htlm build
 * multi thread
 * flask web interface
+
+# Links
+
+* https://stackoverflow.com/questions/6225763/downloading-multiple-attachments-using-imaplib
+* https://gist.github.com/martinrusev/6121028
+* https://gist.github.com/johnpaulhayes/106c3e40dc04b6a6b516
--- run.py
+++ run.py
@@ -6,9 +6,7 @@ import imaplib
 import email
 import traceback
 
-# https://stackoverflow.com/questions/6225763/downloading-multiple-attachments-using-imaplib
-# https://gist.github.com/martinrusev/6121028
-# https://gist.github.com/johnpaulhayes/106c3e40dc04b6a6b516
+
 # -------------------------------------------------
 #
 # Utility to read email from Gmail Using Python

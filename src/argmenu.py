0bd27838c45a7fb1dc5ec5ec446abc32cb0210f8
Alexandre <alexandre@clusterlab.com.br>
2022-01-29 02:06:20 -0300

    added more options to menu

--- src/argmenu.py
+++ src/argmenu.py
@@ -5,6 +5,8 @@ class ArgMenu:
     def __init__(self):
         self.parser = argparse.ArgumentParser(description="GMAIL Archive or any other mail account with IMAP")
         self.parser.add_argument('--cred', type=str, help="Path to a credential json")
+        self.parser.add_argument('--mails', type=str, help="Number of mails to fetch")
+        self.parser.add_argument('--search', type=str, help="Search patter to get mail. https://gist.github.com/martinrusev/6121028#file-imap-search")
         self.parser.add_argument("--excel", type=str, help="Generate excel file")
         self.parser.add_argument("--html", type=str, help="Generate HTML file")
         self.parser.add_argument("--webserver", type=str, help="Start WEBSERVER")
